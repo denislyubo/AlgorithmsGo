7a96a187bdae5ee415b08b1003112bb4c9eb25da
Denis Lyubo <denis.lyubo@gmail.com>
2022-10-12 17:31:32 +0300

    fixed bug in test

--- TwoNumberSumEqualsThird/main_test.go
+++ TwoNumberSumEqualsThird/main_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestTwoNumbersSumEquals(t *testing.T) {
 	i, j, _ := TwoNumbersSumEquals([]int{3, 4, 2, 56, 12, 9, 3, 4}, 68)
 
-	if (i != 56 && j != 12) || (i != 12 && j != 56) {
+	if (i != 56 && j != 12) && (i != 12 && j != 56) {
 		t.Error("error")
 	}
 
