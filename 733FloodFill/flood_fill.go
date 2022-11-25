4f176974b0baea427630f6402c366477012ee539
Denis Lyubo <denis.lyubo@gmail.com>
2022-11-25 16:30:28 +0200

    733 recursive no copy for result no visit matrix

--- 733FloodFill/flood_fill.go
+++ 733FloodFill/flood_fill.go
@@ -4,38 +4,20 @@ func floodFill(image [][]int, sr int, sc int, color int) [][]int {
 	numRows := len(image)
 	numCols := len(image[0])
 
-	res := make([][]int, numRows)
-	visited := make([][]bool, numRows)
-	for i := 0; i < len(res); i++ {
-		res[i] = make([]int, numCols)
-		visited[i] = make([]bool, numCols)
-	}
+	fill(image, sr, sc, image[sr][sc], color, numRows, numCols)
 
-	copy(res, image)
-
-	fill(res, image, visited, sr, sc, image[sr][sc], color, numRows, numCols)
-
-	return res
+	return image
 }
 
-func fill(res, image [][]int, visited [][]bool, i int, j int, srColor, color int, nr, nc int) {
-	visited[i][j] = true
-	if srColor == image[i][j] {
-		res[i][j] = color
-	} else {
+func fill(image [][]int, i, j, srColor, color, nr, nc int) {
+
+	if i < 0 || i >= nr || j < 0 || j >= nc || image[i][j] == color || image[i][j] != srColor {
 		return
 	}
+	image[i][j] = color
 
-	if i-1 >= 0 && !visited[i-1][j] {
-		fill(res, image, visited, i-1, j, srColor, color, nr, nc)
-	}
-	if i+1 < nr && !visited[i+1][j] {
-		fill(res, image, visited, i+1, j, srColor, color, nr, nc)
-	}
-	if j-1 >= 0 && !visited[i][j-1] {
-		fill(res, image, visited, i, j-1, srColor, color, nr, nc)
-	}
-	if j+1 < nc && !visited[i][j+1] {
-		fill(res, image, visited, i, j+1, srColor, color, nr, nc)
-	}
+	fill(image, i-1, j, srColor, color, nr, nc)
+	fill(image, i+1, j, srColor, color, nr, nc)
+	fill(image, i, j-1, srColor, color, nr, nc)
+	fill(image, i, j+1, srColor, color, nr, nc)
 }
