ee447d2070e7b20d69f6db20c50d5dee65965b95
Denis Lyubo <denis.lyubo@gmail.com>
2023-04-26 11:06:43 +0300

    btree added path sum to leaf

---
+++ 112PathSum/path_sum_test.go
@@ -0,0 +1,44 @@
+package pathsum
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestHasPathSum(t *testing.T) {
+	tests := map[string]struct {
+		root     *TreeNode
+		sum      int
+		expected bool
+	}{
+		`first`: {
+			root:     &TreeNode{Val: 1, Left: &TreeNode{Val: 3, Left: &TreeNode{Val: 5}}, Right: &TreeNode{Val: 2}},
+			sum:      9,
+			expected: true,
+		},
+		`second`: {
+			root:     &TreeNode{Val: 1},
+			sum:      1,
+			expected: true,
+		},
+		`third`: {
+			root:     &TreeNode{Val: 2},
+			sum:      1,
+			expected: false,
+		},
+		`forth`: {
+			root:     &TreeNode{Val: 1, Left: &TreeNode{Val: 1, Left: &TreeNode{Val: 2}}, Right: &TreeNode{Val: 2}},
+			sum:      4,
+			expected: true,
+		},
+	}
+
+	for name, tt := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := hasPathSum(tt.root, tt.sum)
+			if !reflect.DeepEqual(got, tt.expected) {
+				t.Errorf("got: %v, expected: %v", got, tt.expected)
+			}
+		})
+	}
+}
